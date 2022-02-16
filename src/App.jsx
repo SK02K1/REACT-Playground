670c7576d2b8ca55c3f9efe378866024dfef3307
Sourabh Kheraliya <55895224+SK02K1@users.noreply.github.com>
2022-02-16 15:50:12 +0530

    add: ex05 solution added toggle password component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { ConfirmPass } from "./components/ConfirmPass";
+import { TogglePassword } from "./components/TogglePassword";
 import { Tweet } from "./components/Tweet";
 import "./styles.css";
 
@@ -7,6 +8,7 @@ export default function App() {
     <div className="App">
       <Tweet />
       <ConfirmPass />
+      <TogglePassword />
     </div>
   );
 }
---
+++ src/components/TogglePassword.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+export const TogglePassword = () => {
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+  return (
+    <div>
+      <h2>05. Toggle Password</h2>
+      <input
+        type={isPasswordVisible ? "text" : "password"}
+        placeholder="Enter password"
+      />
+      <button onClick={() => setIsPasswordVisible((prevState) => !prevState)}>
+        {isPasswordVisible ? "Hide Password" : "Show Password"}
+      </button>
+    </div>
+  );
+};
