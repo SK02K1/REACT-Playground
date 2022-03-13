620c79f51c044547c46064ce318339e826fa50bf
Sourabh Kheraliya <55895224+SK02K1@users.noreply.github.com>
2022-03-13 21:22:21 +0530

    refactor: changed addToCart component logic

--- src/components/AddToCart.jsx
+++ src/components/AddToCart.jsx
@@ -1,32 +1,38 @@
 import { useState } from "react";
 
-const items = ["Eggs", "Bread", "Vada Pav", "Milk", "Paneer"];
+const items = [
+  { id: 1, name: "Eggs" },
+  { id: 2, name: "Bread" },
+  { id: 3, name: "Vada Pav" },
+  { id: 4, name: "Milk" },
+  { id: 5, name: "Paneer" }
+];
 
 export const AddToCart = () => {
-  const [cartItems, setCartItems] = useState({});
+  const [cartItems, setCartItems] = useState([]);
 
   const btnClickHandler = (selectedItem) => {
-    setCartItems(
-      cartItems[selectedItem] === undefined
-        ? { ...cartItems, [selectedItem]: { qty: 1 } }
-        : {
-            ...cartItems,
-            [selectedItem]: {
-              ...cartItems[selectedItem],
-              qty: cartItems[selectedItem].qty + 1
-            }
-          }
-    );
+    setCartItems((prevCartItems) => {
+      return prevCartItems.find((itemInfo) => itemInfo.id === selectedItem.id)
+        ? prevCartItems.map((itemInfo) =>
+            itemInfo.id === selectedItem.id
+              ? { ...itemInfo, qty: itemInfo.qty + 1 }
+              : itemInfo
+          )
+        : [...prevCartItems, { ...selectedItem, qty: 1 }];
+    });
   };
 
   return (
     <div className="container">
       <h2>07. Add To Cart</h2>
       <ul>
-        {items.map((item) => (
-          <li key={item}>
-            <span>{item}</span>
-            <button onClick={() => btnClickHandler(item)}>add to cart</button>
+        {items.map((itemInfo) => (
+          <li key={itemInfo.id}>
+            <span>{itemInfo.name}</span>
+            <button onClick={() => btnClickHandler(itemInfo)}>
+              add to cart
+            </button>
           </li>
         ))}
       </ul>
@@ -36,17 +42,20 @@ export const AddToCart = () => {
           🛒
         </span>
       </h3>
+      <p>
+        {cartItems.length === 0
+          ? "cart is empty"
+          : `items in cart: ${cartItems.length}`}
+      </p>
       <ul>
-        {Object.keys(cartItems).length === 0 ? (
-          <h4>Cart is empty</h4>
-        ) : (
-          Object.keys(cartItems).map((item) => (
-            <li key={item}>
-              <span>{item}</span>
-              <span>qty: {cartItems[item].qty}</span>
+        {cartItems.map(({ id, name, qty }) => {
+          return (
+            <li key={id}>
+              <span>{name}</span>
+              <span>{qty}</span>
             </li>
-          ))
-        )}
+          );
+        })}
       </ul>
     </div>
   );
